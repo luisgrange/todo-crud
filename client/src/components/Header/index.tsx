4f3d89126050403f53de733159dfb6d5a5d3c1b3
luisgrange <luisfernando237grange@outlook.com>
2023-02-07 12:02:32 -0300

    Iniciando repositório

---
+++ client/src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.css';
+
+export function Header(){
+    return(
+        <header>
+            <div id='logo' style={{color: '#fff'}}>
+                Lorem...
+            </div>
+        </header>
+    );
+}
